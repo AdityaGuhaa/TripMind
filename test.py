33d435c4efecd98f554868acf4164ffc08679463
Aditya Guha <adityaaguha@gmail.com>
2026-03-12 14:53:29 +0530

    Initial commit

---
+++ test.py
@@ -0,0 +1,10 @@
+from google import genai
+
+client = genai.Client(api_key="Gemini Api Key")
+
+response = client.models.generate_content(
+    model="gemini-2.5-flash",
+    contents="Plan a 2 day trip to Goa"
+)
+
+print(response.text)
